a23728212685447bfb9df6c2c89a5b6572c153e7
andrey <sh00ter@tut.by>
2017-11-16 22:38:44 +0300

    home_debug_linux

--- src/Nod4/RpsAdminBundle/Entity/NewsImg.php
+++ src/Nod4/RpsAdminBundle/Entity/NewsImg.php
@@ -126,13 +126,13 @@ class NewsImg {
     protected function getUploadRootDir() {
         // the absolute directory path where uploaded
         // documents should be saved
-        return __DIR__ . '/../../../../' . $this->getUploadDir();
+        return __DIR__ . '/../../../../web' . $this->getUploadDir();
     }
 
     protected function getUploadDir() {
         // get rid of the __DIR__ so it doesn't screw up
         // when displaying uploaded doc/image in the view.
-        return '/web/uploads/news/images';
+        return '/uploads/news/images';
     }
 
     public function getFile() {
